621573e31bcd919892b55462aa8a6df262176d58
farahsamad <farah_samad@outlook.com>
2024-11-15 00:15:33 +0200

    update page

--- src/app/verifyEmail/page.tsx
+++ src/app/verifyEmail/page.tsx
@@ -3,7 +3,6 @@
 import React, { useState, useEffect } from "react";
 import Link from "next/link";
 import axios from "axios";
-import { url } from "inspector";
 
 function VerifyEmailPage() {
   const [token, setToken] = useState("");
